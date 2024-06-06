3a8fcaf3f35311b9b28bcc2d283e3974117e04c4
Tejas Sridhar <tejas3098@gmail.com>
2024-06-06 12:46:51 -0400

    added minting logic for laos polygon NFT

--- dapp/src/ifps-uploader.js
+++ dapp/src/ifps-uploader.js
@@ -28,7 +28,7 @@ async function uploadToIPFS(filePath) {
 
 async function main() {
   // Example usage for uploading an image
-  const imagePath = '/Users/loganchoi/Desktop/cryptopolis/test.txt'; // Path to the image file
+  const imagePath = '/Users/tejas/Documents/cryptopolis-tejas/cryptopolis/cryptopoly.png'; // Path to the image file
   const imageHash = await uploadToIPFS(imagePath); // Upload image
 
   if (imageHash) {
@@ -47,10 +47,10 @@ async function main() {
     // };
 
     const metadata = {
-      name: 'Cryptopolis',
+      name: 'Cryptopoly',
       description: 'Save State',
       image: `ipfs://${imageHash}`, // Link image hash in metadata
-      hex: '0x0'
+      string: '0,9,23,1,13,23,2,11,27,9,6,28,9,6,29,9,7,30,9,8,31,9,9,31,9,10,31,9,11,31,9,12,31,9,13,31,9,14,31,9,15,30,9,16,29,9,16,28'
     };
 
     // Save metadata to a file
